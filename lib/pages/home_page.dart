08d15a703d55d2226cc011e2bf699c3d78802966
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-07-03 17:52:28 +0500

    imp

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,8 +8,45 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool isDark = true;
+  int counter = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Center(
+          child: SizedBox(
+            width: 450,
+            height: 450,
+            child: Stack(
+              children: [
+                Image.asset(
+                  "assets/frame.png",
+                  width: 450,
+                  height: 450,
+                ),
+                Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 80,
+                        width: 250,
+                        decoration: BoxDecoration(
+                          color: Colors.white.withOpacity(.7),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
